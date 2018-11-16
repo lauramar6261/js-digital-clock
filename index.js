ae07cbef45d16539429a664f01797f1041a244ac
Ada <larmartinez2@gmail.com>
2018-11-16 15:06:34 -0800

    update time

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 
 $(document).ready(function() {
-  let time = new Date()
-  let hours = time.getHours()
-  let min = time.getMinutes()
-  let description
-  if (hours > 12) {
-    description = "pm"
-  } else {
-    description = "am"
+  let myVar = setInterval(myTimer, 1000);
+  function myTimer() {
+    let time = new Date()
+    let hours = time.getHours()
+    let min = time.getMinutes()
+    if (min < 10) {
+      min = "0" + min
+    }
+    let description
+    if (hours > 12) {
+      description = "pm"
+    } else {
+      description = "am"
+    }
+    $('#clock').empty().append(hours + ":" + min + " " + description)
   }
-  $('#clock').append(hours + ":" + min + " " + description)
 });
